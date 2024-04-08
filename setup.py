a214c4007c7f59f2e6bd6b2711bd6dcde48e1c8e
akshats1 <akshats063@gmail.com>
2024-04-09 02:27:13 +0530

    code updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Breast Cancer Using Pytorch",
+    version="0.0.1",
+    author="Akshat",
+    author_email="akshats063@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
